8cc8fb997689a11e58a9eca2788842ccb046909e
zhi zhou <zhi@zhidemacbook-pro.local>
2016-11-01 22:02:14 +0800

    full

---
+++ SubStringWithRange/SubStringWithRange/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  SubStringWithRange
+//
+//  Created by zhi zhou on 2016/11/1.
+//  Copyright © 2016年 zhi zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let string = "<a href= http://weibo.com rel= nofollow>新浪微博</a>"
+        
+        let range = string.range(of: ">")
+        let startIndex = range!.upperBound
+        
+        let range1 = Range(uncheckedBounds: (lower: string.startIndex, upper: string.endIndex))
+        let endRange = string.range(of: "<", options: .backwards, range: range1, locale: Locale(identifier: "<"))
+        let endIndex = endRange!.lowerBound
+        
+        
+        
+        let searchRange = Range(uncheckedBounds: (startIndex, endIndex))
+        
+        let endString = string.substring(with: searchRange)
+        print(endString)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
